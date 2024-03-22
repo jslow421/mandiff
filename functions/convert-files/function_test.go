dbc41f061ebd54ea1469b323636f3f338dbd1f83
John Slowik <john.slowik@caylent.com>
2024-03-22 12:53:03 -0500

    Fix tests

--- functions/convert-files/function_test.go
+++ functions/convert-files/function_test.go
@@ -4,14 +4,9 @@ import (
 	"testing"
 )
 
-// Test the checkLanguage function
 func TestIdentifyEnglishLineOfText(t *testing.T) {
-	// Call the checkLanguage function
-	isEnglish, err := checkIfTextIsLikelyEnglish("Mount the Ranger pressure infusor Model 145 on only a 3M model 90068/90124 pressure infusor I.V. pole/base")
+	isEnglish := checkIfTextIsLikelyEnglish("Mount the Ranger pressure infusor Model 145 on only a 3M model 90068/90124 pressure infusor I.V. pole/base")
 
-	if err != nil {
-		t.Errorf("Error calling checkLanguage: %v", err)
-	}
 	expected := true
 
 	if isEnglish != expected {
@@ -20,12 +15,8 @@ func TestIdentifyEnglishLineOfText(t *testing.T) {
 }
 
 func TestIdentifyingNonEnglishText(t *testing.T) {
-	// Call the checkLanguage function
-	isEnglish, err := checkIfTextIsLikelyEnglish("Installez le dispositif de perfusion sous pression Ranger, modèle 145, sur une potence/base I.V. 3M, modèle 90068/90124")
+	isEnglish := checkIfTextIsLikelyEnglish("Installez le dispositif de perfusion sous pression Ranger, modèle 145, sur une potence/base I.V. 3M, modèle 90068/90124")
 
-	if err != nil {
-		t.Errorf("Error calling checkLanguage: %v", err)
-	}
 	expected := false
 
 	if isEnglish != expected {
